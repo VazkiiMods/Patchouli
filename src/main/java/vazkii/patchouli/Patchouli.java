5398507b1ac1e669dcb41eca76797dbeb52e426e
Ellpeck <megamaximal@gmail.com>
2018-10-23 16:40:54 +0200

    remove ARL dependency and fix missing error locale

--- src/main/java/vazkii/patchouli/Patchouli.java
+++ src/main/java/vazkii/patchouli/Patchouli.java
@@ -9,12 +9,11 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import vazkii.patchouli.common.base.CommonProxy;
 
-// TODO wipe all alquimia lang keys
 // TODO rewrite book registry to support multiple books, remove any singleton/static references
 // TODO port multiblock system
 // TODO port right click handler
 
-@Mod(modid = Patchouli.MOD_ID, name = Patchouli.MOD_NAME, dependencies = Patchouli.DEPENDENCIES)
+@Mod(modid = Patchouli.MOD_ID, name = Patchouli.MOD_NAME)
 public class Patchouli {
 
 	// Mod Constants
@@ -22,7 +21,6 @@ public class Patchouli {
 	public static final String MOD_NAME = "Patchouli";
 	public static final String BUILD = "GRADLE:BUILD";
 	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-	public static final String DEPENDENCIES = "required-before:autoreglib;";
 	public static final String PREFIX_MOD = MOD_ID + ":";
 	
 	public static final String COMMON_PROXY = "vazkii.patchouli.common.base.CommonProxy";
