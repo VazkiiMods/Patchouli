8a0f86521bfb88ba39888143e935c018a08a4f3d
Vasco Lavos <vazkii@hotmail.com>
2018-10-26 01:31:03 +0100

    Re-inflate the zip

--- src/main/java/vazkii/patchouli/common/book/BookRegistry.java
+++ src/main/java/vazkii/patchouli/common/book/BookRegistry.java
@@ -1,9 +1,9 @@
 package vazkii.patchouli.common.book;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,52 +29,64 @@ public class BookRegistry {
 
 	public final Map<ResourceLocation, Book> books = new HashMap();
 	public Gson gson;
-	
+
 	private BookRegistry() { 
 		gson = new GsonBuilder().create();
 	}
 
 	public void init() {
 		List<ModContainer> mods = Loader.instance().getActiveModList();
-		Map<Pair<ModContainer, ResourceLocation>, Reader> foundBooks = new HashMap();
+		Map<Pair<ModContainer, ResourceLocation>, Path> foundBooks = new HashMap();
 
 		// TODO remove debugs
 		mods.forEach((mod) -> {
 			String id = mod.getModId();
 			CraftingHelper.findFiles(mod, String.format("assets/%s/%s", id, BOOKS_LOCATION), (path) -> Files.exists(path),
-			(path, file) -> {
-				if(file.toString().endsWith("book.json")) {
-					String fileStr = file.toString().replaceAll("\\\\", "/");
-					System.out.println("fileStr: " + fileStr);
-					String relPath = fileStr.substring(fileStr.indexOf(BOOKS_LOCATION) + BOOKS_LOCATION.length() + 1);
-					System.out.println("relPath: " + relPath);
-					String bookName = relPath.substring(0, relPath.indexOf("/"));
-					System.out.println("bookName: " + bookName);
-					
-					if(bookName.indexOf("/") > -1) {
-						(new IllegalArgumentException("Ignored book.json @ " + file)).printStackTrace();
-						return true;
-					}
-					
-					ResourceLocation bookId = new ResourceLocation(id, bookName);
-					try {
-						System.out.println("bookId: " + bookId);
-						foundBooks.put(Pair.of(mod, bookId), Files.newBufferedReader(file));
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
+					(path, file) -> {
+						if(file.toString().endsWith("book.json")) {
+							String fileStr = file.toString().replaceAll("\\\\", "/");
+							System.out.println("fileStr: " + fileStr);
+							String relPath = fileStr.substring(fileStr.indexOf(BOOKS_LOCATION) + BOOKS_LOCATION.length() + 1);
+							System.out.println("relPath: " + relPath);
+							String bookName = relPath.substring(0, relPath.indexOf("/"));
+							System.out.println("bookName: " + bookName);
 
-				return true;
-			}, false, true);
+							if(bookName.indexOf("/") > -1) {
+								(new IllegalArgumentException("Ignored book.json @ " + file)).printStackTrace();
+								return true;
+							}
+
+							ResourceLocation bookId = new ResourceLocation(id, bookName);
+							foundBooks.put(Pair.of(mod, bookId), file);
+						}
+
+						return true;
+					}, false, true);
 		});
-		
-		foundBooks.forEach((pair, reader) -> {
-			Book book = gson.fromJson(reader, Book.class);
-			books.put(pair.getRight(), book);
-			
-			book.build(pair.getLeft(), pair.getRight());
+
+		// Using findFiles again ensures the zip is inflated again
+		mods.forEach((mod) -> {
+			CraftingHelper.findFiles(mod, String.format("assets/%s/%s", mod.getModId(), BOOKS_LOCATION), (path) -> Files.exists(path),
+					(path, file) -> {
+						foundBooks.forEach((pair, bookFile) -> {
+							if(pair.getLeft() == mod) {
+								try {
+									Reader reader = Files.newBufferedReader(bookFile);
+									Book book = gson.fromJson(reader, Book.class);
+									System.out.println(book);
+									books.put(pair.getRight(), book);
+
+									book.build(pair.getLeft(), pair.getRight());
+								} catch(IOException e) {
+									throw new RuntimeException("Failed to load book", e);
+								}
+							}
+						});
+						
+						return false;
+					});
 		});
+
 	}
 
 	@SideOnly(Side.CLIENT)
