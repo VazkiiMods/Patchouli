abe15d23b13e5a63bb78836a50ecbca195a64071
Vasco Lavos <vazkii@hotmail.com>
2018-11-08 17:47:36 +0000

    Add draw_filler to the empty page

--- src/main/java/vazkii/patchouli/client/book/page/PageEmpty.java
+++ src/main/java/vazkii/patchouli/client/book/page/PageEmpty.java
@@ -1,13 +1,19 @@
 package vazkii.patchouli.client.book.page;
 
+import com.google.gson.annotations.SerializedName;
+
 import vazkii.patchouli.client.book.BookPage;
 import vazkii.patchouli.client.book.gui.GuiBook;
 
 public class PageEmpty extends BookPage {
 
+	@SerializedName("draw_filler")
+	boolean filler = true;
+	
 	@Override
 	public void render(int mouseX, int mouseY, float pticks) {
-		GuiBook.drawPageFiller(book, 0, 0);
+		if(filler)
+			GuiBook.drawPageFiller(book, 0, 0);
 	}
 
 }
