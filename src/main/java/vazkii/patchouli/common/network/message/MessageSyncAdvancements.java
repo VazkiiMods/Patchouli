499311a321c216f2275978f999a546f88f95820d
Vincent Lee <vincent.lee@outlook.com>
2020-01-27 20:31:08 -0600

    Simplify networking impl instead of the copied ARL stuff from before

--- src/main/java/vazkii/patchouli/common/network/message/MessageSyncAdvancements.java
+++ src/main/java/vazkii/patchouli/common/network/message/MessageSyncAdvancements.java
@@ -1,30 +1,47 @@
 package vazkii.patchouli.common.network.message;
 
+import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
 import vazkii.patchouli.client.base.ClientAdvancements;
-import vazkii.patchouli.common.network.IMessage;
 
-public class MessageSyncAdvancements implements IMessage {
+import java.util.function.Supplier;
 
-	private static final long serialVersionUID = 6052845538836764260L;
+public class MessageSyncAdvancements {
+
+	public final String[] done;
+	public final boolean showToast;
 	
-	public String[] done;
-	public boolean showToast;
-	
-	public MessageSyncAdvancements() { }
+	public MessageSyncAdvancements(PacketBuffer buf) {
+		int count = buf.readVarInt();
+		done = new String[count];
+		for (int i = 0; i < count; i++) {
+			done[i] = buf.readString();
+		}
+		showToast = buf.readBoolean();
+	}
 	
 	public MessageSyncAdvancements(String[] done, boolean showToast) { 
 		this.done = done;
 		this.showToast = showToast;
 	}
+
+	public void encode(PacketBuffer buf) {
+		buf.writeVarInt(done.length);
+		for (String s : done) {
+			buf.writeString(s);
+		}
+		buf.writeBoolean(showToast);
+	}
 	
-	@Override
-	public boolean receive(NetworkEvent.Context context) {
-		context.enqueueWork(() -> {
-			ClientAdvancements.setDoneAdvancements(done, showToast, false);
-		});
-		
-		return true;
+	public boolean receive(Supplier<NetworkEvent.Context> context) {
+		if (context.get().getDirection().getReceptionSide().isClient()) {
+			context.get().enqueueWork(() -> {
+				ClientAdvancements.setDoneAdvancements(done, showToast, false);
+			});
+			return true;
+		}
+
+		return false;
 	}
 	
 }
