549e974d9df58d324a315fba51cde8e3248de073
Minecraftschurli <minecraftschurli@gmail.com>
2020-05-06 22:45:36 +0200

    fix spotless issues
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/main/java/vazkii/patchouli/api/PatchouliAPI.java
+++ src/main/java/vazkii/patchouli/api/PatchouliAPI.java
@@ -272,12 +272,14 @@ public class PatchouliAPI {
 
 		/**
 		 * Registers a command macro handler under the given names
+		 *
 		 * @throws IllegalArgumentException if a macro with one of the given names was already registered
 		 */
 		void registerMacro(ICommandProcessor handler, String... names);
 
 		/**
 		 * Registers a function macro handler under the given names
+		 *
 		 * @throws IllegalArgumentException if a macro with one of the given names was already registered
 		 */
 		void registerMacro(IFunctionProcessor handler, String... names);
