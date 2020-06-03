a253a7183018fd81a771c9e0ef1d7b72a85cfeed
Hubry <Hubry@users.noreply.github.com>
2020-06-04 01:31:54 +0200

    A few 1.12 backports (#249)
    * Update Gradle wrapper
    
    * Backport recipe book fix
    
    (based on 4b011f9d6abd81620c76c34807cf56d28163c726)
    
    * Add crash report diagnostics
    
    (cherry picked from commit a3a6bda18e718002296c03a13aed9a80deb9032f)
    
    * Switch advancement locking to pull from vanilla advancement manager
    
    (based on 0a17d60dc1322b8f5180b54696fa756dae7f5774, but does not use
    a coremod. Instead sends another packet after advancement event
    is triggered, up to 1 per tick. The packet does not contain any data
    and is purely used as a notification mechanism.)
    In addition, prevent duplicate book toasts from appearing.

--- src/main/java/vazkii/patchouli/client/base/ClientAdvancements.java
+++ src/main/java/vazkii/patchouli/client/base/ClientAdvancements.java
@@ -1,36 +1,50 @@
 package vazkii.patchouli.client.base;
 
-import java.util.Arrays;
-import java.util.List;
-
+import com.google.common.base.Throwables;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.toasts.GuiToast;
 import net.minecraft.client.gui.toasts.IToast;
+import net.minecraft.client.multiplayer.ClientAdvancementManager;
+import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 import vazkii.patchouli.client.book.ClientBookRegistry;
 import vazkii.patchouli.common.base.PatchouliConfig;
 import vazkii.patchouli.common.book.Book;
 import vazkii.patchouli.common.book.BookRegistry;
 
+import javax.annotation.Nonnull;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+import java.util.Map;
+
 public class ClientAdvancements {
+	private static final MethodHandle ADVANCEMENT_TO_PROGRESS;
 
-	static List<String> doneAdvancements;
+	static {
+		Field field = ObfuscationReflectionHelper.findField(ClientAdvancementManager.class, "field_192803_d");
+		try {
+			ADVANCEMENT_TO_PROGRESS = MethodHandles.lookup().unreflectGetter(field);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Failed to reflect advancement progress map field", e);
+		}
+	}
 
-	public static void setDoneAdvancements(String[] done, boolean showToast, boolean reset) {
-		showToast &= !PatchouliConfig.disableAdvancementLocking;
-
-		doneAdvancements = Arrays.asList(done);
+	public static void setDoneAdvancements(boolean showToast, boolean reset) {
 		updateLockStatus(reset);
 
-		if(showToast)
+		if (showToast && !PatchouliConfig.disableAdvancementLocking)
 			BookRegistry.INSTANCE.books.values().forEach(b -> {
-				if(b.popUpdated() && b.showToasts) {
-					Minecraft.getMinecraft().getToastGui().add(new LexiconToast(b));
+				if (b.popUpdated() && b.showToasts) {
+					sendBookToast(b);
 				}
 			});
 	}
@@ -39,31 +53,60 @@ public class ClientAdvancements {
 		ClientBookRegistry.INSTANCE.reloadLocks(reset);
 	}
 
-	public static void resetIfNeeded() {
-		if(doneAdvancements != null && doneAdvancements.size() > 0)
-			setDoneAdvancements(new String[0], false, true);
+	public static boolean hasDone(String advancement) {
+		ResourceLocation id = new ResourceLocation(advancement);
+		NetHandlerPlayClient conn = Minecraft.getMinecraft().getConnection();
+		if (conn != null) {
+			ClientAdvancementManager manager = conn.getAdvancementManager();
+			Advancement adv = manager.getAdvancementList().getAdvancement(id);
+			if (adv != null) {
+				AdvancementProgress progress = getProgress(manager, adv);
+				return progress != null && progress.isDone();
+			}
+		}
+		return false;
 	}
 
-	public static boolean hasDone(String advancement) {
-		return doneAdvancements != null && doneAdvancements.contains(advancement);
+	private static AdvancementProgress getProgress(ClientAdvancementManager manager, Advancement advancement) {
+		try {
+			@SuppressWarnings("unchecked")
+			Map<Advancement, AdvancementProgress> map = (Map<Advancement, AdvancementProgress>) ADVANCEMENT_TO_PROGRESS.invokeExact(manager);
+			return map.get(advancement);
+		} catch (Throwable t) {
+			Throwables.throwIfUnchecked(t);
+			throw new RuntimeException("Failed to reflect advancement progress map!", t);
+		}
 	}
 
 	@SubscribeEvent
-	public static void onTick(ClientTickEvent event) {
-		if(event.phase == Phase.END && Minecraft.getMinecraft().player == null)
-			resetIfNeeded();
+	public static void onLogout(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
+		updateLockStatus(true);
+	}
+
+	private static void sendBookToast(Book book) {
+		GuiToast toastGui = Minecraft.getMinecraft().getToastGui();
+		LexiconToast toast = toastGui.getToast(LexiconToast.class, book);
+		if (toast == null) {
+			toastGui.add(new LexiconToast(book));
+		}
 	}
 
 	public static class LexiconToast implements IToast {
-
-		final Book book;
+		private final Book book;
 
 		public LexiconToast(Book book) {
 			this.book = book;
 		}
 
+		@Nonnull
 		@Override
-		public Visibility draw(GuiToast toastGui, long delta) {
+		public Book getType() {
+			return book;
+		}
+
+		@Nonnull
+		@Override
+		public Visibility draw(@Nonnull GuiToast toastGui, long delta) {
 			Minecraft mc = Minecraft.getMinecraft();
 			mc.getTextureManager().bindTexture(TEXTURE_TOASTS);
 			GlStateManager.color(1.0F, 1.0F, 1.0F);
@@ -73,7 +116,7 @@ public class ClientAdvancements {
 			toastGui.getMinecraft().fontRenderer.drawString(I18n.format("patchouli.gui.lexicon.toast.info"), 30, 17, -16777216);
 
 			RenderHelper.enableGUIStandardItemLighting();
-			toastGui.getMinecraft().getRenderItem().renderItemAndEffectIntoGUI(null, book.getBookItem(), 8, 8); 
+			toastGui.getMinecraft().getRenderItem().renderItemAndEffectIntoGUI(null, book.getBookItem(), 8, 8);
 
 			return delta >= 5000L ? IToast.Visibility.HIDE : IToast.Visibility.SHOW;
 		}
