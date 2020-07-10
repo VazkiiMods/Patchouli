5efdf706dd5226c18d2f022037a6bb346a9b1129
Vincent Lee <vincent.lee@outlook.com>
2020-07-09 21:45:08 -0500

    Extra safety checks in modelbakeevent so people stop bothering us with issues that aren't our fault.

--- src/main/java/vazkii/patchouli/common/item/PatchouliItems.java
+++ src/main/java/vazkii/patchouli/common/item/PatchouliItems.java
@@ -3,6 +3,7 @@ package vazkii.patchouli.common.item;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelBakeEvent;
@@ -20,7 +21,8 @@ import vazkii.patchouli.common.book.BookRegistry;
 @EventBusSubscriber(bus = Bus.MOD)
 public class PatchouliItems {
 
-	public static final Item book = new ItemModBook();
+	private static final ResourceLocation BOOK_ID = new ResourceLocation(Patchouli.MOD_ID, "guide_book");
+	public static final Item book = new ItemModBook().setRegistryName(BOOK_ID);
 
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
@@ -33,13 +35,15 @@ public class PatchouliItems {
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public static void replaceModel(ModelBakeEvent event) {
-		ModelResourceLocation key = new ModelResourceLocation(book.getRegistryName(), "inventory");
+		ModelResourceLocation key = new ModelResourceLocation(BOOK_ID, "inventory");
 		IBakedModel oldModel = event.getModelRegistry().get(key);
-		event.getModelRegistry().put(key, new BookModel(oldModel));
+		if (oldModel != null) {
+			event.getModelRegistry().put(key, new BookModel(oldModel));
+		}
 	}
 
 	@SubscribeEvent
 	public static void register(RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(book.setRegistryName(Patchouli.MOD_ID, "guide_book"));
+		event.getRegistry().register(book);
 	}
 }
