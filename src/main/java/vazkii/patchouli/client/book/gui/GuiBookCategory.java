d60c0ec216f6aac945926580657330f9b1e9d437
Hubry <26120076+Hubry@users.noreply.github.com>
2020-12-22 23:03:08 +0100

    Move subcategory buttons to the right if the category has no entries
    Mild positioning tweaks to category buttons to leave more space for
    the description (they wasted quite a bit of space with advancement
    locks disabled)

--- src/main/java/vazkii/patchouli/client/book/gui/GuiBookCategory.java
+++ src/main/java/vazkii/patchouli/client/book/gui/GuiBookCategory.java
@@ -1,7 +1,11 @@
 package vazkii.patchouli.client.book.gui;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.resources.I18n;
 
 import vazkii.patchouli.client.book.BookCategory;
 import vazkii.patchouli.client.book.BookEntry;
@@ -12,7 +16,8 @@ import java.util.*;
 
 public class GuiBookCategory extends GuiBookEntryList {
 
-	BookCategory category;
+	private final BookCategory category;
+	private int subcategoryButtonCount;
 
 	public GuiBookCategory(Book book, BookCategory category) {
 		super(book, category.getName());
@@ -30,17 +35,35 @@ public class GuiBookCategory extends GuiBookEntryList {
 	}
 
 	@Override
+	void drawForegroundElements(MatrixStack ms, int mouseX, int mouseY, float partialTicks) {
+		super.drawForegroundElements(ms, mouseX, mouseY, partialTicks);
+		if (getEntries().isEmpty() && subcategoryButtonCount <= 16 && subcategoryButtonCount > 0) {
+			int bottomSeparator = TOP_PADDING + 37 + 24 * ((subcategoryButtonCount - 1) / 4 + 1);
+			drawSeparator(ms, book, RIGHT_PAGE_X, bottomSeparator);
+		}
+	}
+
+	@Override
 	protected void addSubcategoryButtons() {
 		int i = 0;
 		List<BookCategory> categories = new ArrayList<>(book.contents.categories.values());
 		categories.removeIf(cat -> cat.getParentCategory() != category || cat.shouldHide());
 		Collections.sort(categories);
+		subcategoryButtonCount = categories.size();
 
-		int baseY = TOP_PADDING + PAGE_HEIGHT - (categories.size() / 4) * 20 - (!book.advancementsEnabled() ? 46 : 68);
+		int baseX, baseY;
+		boolean rightPageFree = getEntries().isEmpty();
+		if (rightPageFree) {
+			baseX = RIGHT_PAGE_X + 10;
+			baseY = TOP_PADDING + 25;
+		} else {
+			baseX = LEFT_PAGE_X + 10;
+			baseY = TOP_PADDING + PAGE_HEIGHT - (categories.size() / 4) * 20 - (!book.advancementsEnabled() ? 38 : 64);
+		}
 
 		for (BookCategory ocategory : categories) {
-			int x = LEFT_PAGE_X + 10 + (i % 4) * 24;
-			int y = baseY + (i / 4) * 20;
+			int x = baseX + (i % 4) * 24;
+			int y = baseY + (i / 4) * (rightPageFree ? 24 : 20);
 
 			Button button = new GuiButtonCategory(this, x, y, ocategory, this::handleButtonCategory);
 			addButton(button);
@@ -48,6 +71,33 @@ public class GuiBookCategory extends GuiBookEntryList {
 
 			i++;
 		}
+	}
+
+	@Override
+	protected String getChapterListTitle() {
+		if (getEntries().isEmpty() && subcategoryButtonCount > 0) {
+			return I18n.format("patchouli.gui.lexicon.categories");
+		}
+		return super.getChapterListTitle();
+	}
+
+	@Override
+	protected String getNoEntryMessage() {
+		if (subcategoryButtonCount > 0) {
+			return "";
+		}
+		return super.getNoEntryMessage();
+	}
+
+	@Override
+	protected TextFieldWidget createSearchBar() {
+		TextFieldWidget widget = super.createSearchBar();
+		if (getEntries().isEmpty()) {
+			widget.active = false;
+			widget.setEnabled(false);
+			widget.setVisible(false);
+		}
+		return widget;
 	}
 
 	@Override
