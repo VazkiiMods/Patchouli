90cd44c1bff448cd81a23a0c1a8a39428c67a4a5
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-03-02 09:12:50 -0600

    Changes requested by williewillus

--- src/main/java/vazkii/patchouli/client/book/text/SpanState.java
+++ src/main/java/vazkii/patchouli/client/book/text/SpanState.java
@@ -9,6 +9,8 @@ import net.minecraft.util.text.Style;
 import vazkii.patchouli.client.book.gui.GuiBook;
 import vazkii.patchouli.common.book.Book;
 
+import javax.annotation.Nullable;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -20,11 +22,11 @@ public class SpanState {
 	public final GuiBook gui;
 	public final Book book;
 
-	private final Style baseStyle;
-	private final Deque<Style> styleStack = new ArrayDeque<>();
-	// This keeps a list of all modifications made to the style at each layer,
+	private Style baseStyle;
+	// In addition to caching the computed style for quick lookup,
+	// this keeps a list of all modifications made to the style at each layer,
 	// so that when we replace the base style, we can reconstruct the style stack.
-	private final Deque<SpanPartialState> modifiersStack = new ArrayDeque<>();
+	private final Deque<SpanPartialState> stateStack = new ArrayDeque<>();
 	public IFormattableTextComponent tooltip = BookTextParser.EMPTY_STRING_COMPONENT;
 	public Supplier<Boolean> onClick = null;
 	public List<Span> cluster = null;
@@ -39,8 +41,7 @@ public class SpanState {
 		this.gui = gui;
 		this.book = book;
 		this.baseStyle = baseStyle;
-		this.styleStack.push(baseStyle);
-		this.modifiersStack.push(new SpanPartialState(null));
+		this.stateStack.push(new SpanPartialState(baseStyle, null));
 		this.spaceWidth = Minecraft.getInstance().fontRenderer.getStringPropertyWidth(new StringTextComponent(" ").setStyle(baseStyle));
 	}
 
@@ -49,10 +50,10 @@ public class SpanState {
 	}
 
 	public void changeBaseStyle(Style newStyle) {
-		styleStack.clear();
-		for (SpanPartialState state : modifiersStack) {
-			newStyle = state.reroll(newStyle);
-			styleStack.push(newStyle);
+		baseStyle = newStyle;
+		for (SpanPartialState state : stateStack) {
+			state.replaceBase(newStyle);
+			newStyle = state.getCurrentStyle();
 		}
 	}
 
@@ -65,32 +66,24 @@ public class SpanState {
 	}
 
 	public void modifyStyle(UnaryOperator<Style> f) {
-		Style top = styleStack.pop();
-		styleStack.push(f.apply(top));
-		modifiersStack.peek().addModification(f);
+		stateStack.peek().addModification(f);
 	}
 
 	public void pushStyle(Style style) {
-		Style top = styleStack.peek();
-		modifiersStack.push(new SpanPartialState(style));
-		styleStack.push(style.mergeStyle(top));
+		stateStack.push(new SpanPartialState(stateStack.peek().getCurrentStyle(), style));
 	}
 
 	public Style popStyle() {
-		if (styleStack.size() <= 1) {
+		if (stateStack.size() <= 1) {
 			throw new IllegalStateException("Underflow in style stack");
 		}
-		Style ret = styleStack.pop();
-		modifiersStack.pop();
-		return ret;
+		return stateStack.pop().getCurrentStyle();
 	}
 
 	public void reset() {
 		endingExternal = isExternalLink;
-		styleStack.clear();
-		styleStack.push(baseStyle);
-		modifiersStack.clear();
-		modifiersStack.push(new SpanPartialState(null));
+		stateStack.clear();
+		stateStack.push(new SpanPartialState(baseStyle, null));
 		cluster = null;
 		tooltip = BookTextParser.EMPTY_STRING_COMPONENT;
 		onClick = null;
@@ -98,18 +91,27 @@ public class SpanState {
 	}
 
 	public Style peekStyle() {
-		return styleStack.getFirst();
+		return stateStack.peek().getCurrentStyle();
 	}
 
-	// Represents the transformations on a single stack frame.
+	// Represents the styling applied to a single stack frame.
 	private class SpanPartialState {
-		// This is null iff this state represents the base state (i.e. nothing to merge)
-		private final Style mergeStyle;
+		// This is the current style of the frame.
+		private Style currentStyle;
+		// This is null iff this frame represents the base state (i.e. nothing to merge),
+		@Nullable private final Style mergeStyle;
 		// List of all the transformations that are applied to this frame.
-		private List<UnaryOperator<Style>> transformations = null;
+		@Nullable private List<UnaryOperator<Style>> transformations = null;
 
-		public SpanPartialState(Style style) {
-			this.mergeStyle = style;
+		// Takes the current style state from downstream
+		// and a style to merge onto it.
+		public SpanPartialState(Style currentStyle, Style mergeStyle) {
+			this.currentStyle = currentStyle;
+			this.mergeStyle = mergeStyle;
+		}
+
+		public Style getCurrentStyle() {
+			return currentStyle;
 		}
 
 		public void addModification(UnaryOperator<Style> f) {
@@ -119,9 +121,10 @@ public class SpanState {
 				transformations = new LinkedList<>();
 			}
 			transformations.add(f);
+			this.currentStyle = f.apply(currentStyle);
 		}
 
-		public Style reroll(Style style) {
+		public void replaceBase(Style style) {
 			if (mergeStyle != null) {
 				style = mergeStyle.mergeStyle(style);
 			}
@@ -130,7 +133,7 @@ public class SpanState {
 					style = f.apply(style);
 				}
 			}
-			return style;
+			this.currentStyle = style;
 		}
 	}
 }
