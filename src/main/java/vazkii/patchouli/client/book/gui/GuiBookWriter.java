751c2cdb6df93eab8ad3b1ea11b58a2699065d2e
Al Winfy <20421383+Alwinfy@users.noreply.github.com>
2021-03-03 20:45:40 -0600

    Public command registration (#371)
    * Add API stuff for registering $() functions/commands
    
    * made requested changes
    
    * Document threadunsafety
    
    * Make cmd/func registration concurrent

--- src/main/java/vazkii/patchouli/client/book/gui/GuiBookWriter.java
+++ src/main/java/vazkii/patchouli/client/book/gui/GuiBookWriter.java
@@ -28,7 +28,7 @@ public class GuiBookWriter extends GuiBook {
 		super.init();
 
 		text = new BookTextRenderer(this, new TranslatableText("patchouli.gui.lexicon.editor.info"), LEFT_PAGE_X, TOP_PADDING + 20);
-		textfield = new TextFieldWidget(font, 10, FULL_HEIGHT - 40, PAGE_WIDTH, 20, LiteralText.EMPTY);
+		textfield = new TextFieldWidget(textRenderer, 10, FULL_HEIGHT - 40, PAGE_WIDTH, 20, LiteralText.EMPTY);
 		textfield.setMaxLength(Integer.MAX_VALUE);
 		textfield.setText(savedText);
 
