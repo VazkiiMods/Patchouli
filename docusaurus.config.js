cd720afc6a543801aedb66c75b39b49c48f66136
Vincent Lee <vincent.lee@outlook.com>
2021-09-02 20:00:52 -0500

    Fix edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,13 +87,13 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/VazkiiMods/Patchouli/edit/documentation/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/VazkiiMods/Patchouli/edit/documentation/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
