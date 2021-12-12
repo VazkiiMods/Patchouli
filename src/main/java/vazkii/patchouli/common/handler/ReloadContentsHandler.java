d91769be18c1310b72e0a3ce59217f791f356130
Hubry <26120076+Hubry@users.noreply.github.com>
2021-12-12 01:59:44 +0100

    Fix /reload not reloading books

--- src/main/java/vazkii/patchouli/common/handler/ReloadContentsHandler.java
+++ src/main/java/vazkii/patchouli/common/handler/ReloadContentsHandler.java
@@ -2,19 +2,21 @@ package vazkii.patchouli.common.handler;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.packs.resources.ReloadableResourceManager;
-import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.server.ServerResources;
 
+import vazkii.patchouli.common.base.Patchouli;
 import vazkii.patchouli.common.network.message.MessageReloadBookContents;
 
 public class ReloadContentsHandler {
 	public static void init() {
-		ServerLifecycleEvents.SERVER_STARTED.register(ReloadContentsHandler::serverStart);
+		ServerLifecycleEvents.END_DATA_PACK_RELOAD.register(ReloadContentsHandler::dataReloaded);
 	}
 
-	private static void serverStart(MinecraftServer server) {
+	private static void dataReloaded(MinecraftServer server, ServerResources serverResourceManager, boolean success) {
 		// Also reload contents when someone types /reload
-		ResourceManagerReloadListener listener = m -> MessageReloadBookContents.sendToAll(server);
-		((ReloadableResourceManager) server.getResourceManager()).registerReloadListener(listener);
+		if (success) {
+			Patchouli.LOGGER.info("Sending reload packet to clients");
+			MessageReloadBookContents.sendToAll(server);
+		}
 	}
 }
