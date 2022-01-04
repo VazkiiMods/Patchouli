b544eba543752490e2848d976bfa71fd86b6a7f7
Hubry <26120076+Hubry@users.noreply.github.com>
2022-01-04 23:26:34 +0100

    Spotless.

--- Fabric/src/main/java/vazkii/patchouli/fabric/common/FiberPatchouliConfig.java
+++ Fabric/src/main/java/vazkii/patchouli/fabric/common/FiberPatchouliConfig.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.List;
