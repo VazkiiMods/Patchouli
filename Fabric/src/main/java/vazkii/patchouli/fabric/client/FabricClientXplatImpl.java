e5586e791a71dfef6b9cf289b201e7f533cae217
Alex O'Neill <35673674+alcatrazEscapee@users.noreply.github.com>
2022-06-12 20:57:56 -0400

    Fix liquid block rendering in multiblock in-page rendering (#552)

--- Fabric/src/main/java/vazkii/patchouli/fabric/client/FabricClientXplatImpl.java
+++ Fabric/src/main/java/vazkii/patchouli/fabric/client/FabricClientXplatImpl.java
@@ -1,14 +1,20 @@
 package vazkii.patchouli.fabric.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 
+import vazkii.patchouli.client.book.LiquidBlockVertexConsumer;
 import vazkii.patchouli.xplat.IClientXplatAbstractions;
 
 import java.util.Random;
@@ -16,9 +22,17 @@ import java.util.Random;
 public class FabricClientXplatImpl implements IClientXplatAbstractions {
 	@Override
 	public void renderForMultiblock(BlockState state, BlockPos pos, BlockAndTintGetter multiblock, PoseStack ps, MultiBufferSource buffers, Random rand) {
-		var layer = ItemBlockRenderTypes.getChunkRenderType(state);
-		var buffer = buffers.getBuffer(layer);
-		Minecraft.getInstance().getBlockRenderer()
-				.renderBatched(state, pos, multiblock, ps, buffer, false, rand);
+		final BlockRenderDispatcher blockRenderer = Minecraft.getInstance().getBlockRenderer();
+		final FluidState fluidState = state.getFluidState();
+		if (!fluidState.isEmpty()) {
+			final RenderType layer = ItemBlockRenderTypes.getRenderLayer(fluidState);
+			final VertexConsumer buffer = buffers.getBuffer(layer);
+			blockRenderer.renderLiquid(pos, multiblock, new LiquidBlockVertexConsumer(buffer, ps, pos), state, fluidState);
+		}
+		if (state.getRenderShape() != RenderShape.INVISIBLE) {
+			final RenderType layer = ItemBlockRenderTypes.getChunkRenderType(state);
+			final VertexConsumer buffer = buffers.getBuffer(layer);
+			blockRenderer.renderBatched(state, pos, multiblock, ps, buffer, false, rand);
+		}
 	}
 }
