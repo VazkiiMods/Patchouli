6a99d65848521253a88e69396b15c03eb77d6a24
Vincent Lee <vincent@vincent-lee.net>
2022-08-07 17:12:06 -0700

    Handle gui scales above 6 properly
    Closes #531. Thanks wire for the idea

--- Xplat/src/main/java/vazkii/patchouli/client/book/gui/button/GuiButtonBookResize.java
+++ Xplat/src/main/java/vazkii/patchouli/client/book/gui/button/GuiButtonBookResize.java
@@ -3,6 +3,7 @@ package vazkii.patchouli.client.book.gui.button;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 import vazkii.patchouli.client.base.PersistentData;
 import vazkii.patchouli.client.book.gui.GuiBook;
@@ -18,9 +19,21 @@ public class GuiButtonBookResize extends GuiButtonBook {
 
 	@Override
 	public List<Component> getTooltip() {
-		return Arrays.asList(
-				tooltip.get(0),
-				Component.translatable("patchouli.gui.lexicon.button.resize.size" + PersistentData.data.bookGuiScale).withStyle(ChatFormatting.GRAY));
+		var size = PersistentData.data.bookGuiScale;
+		// The last size with a dedicated message. For sizes 6 and above, we display
+		// verybig.message, wrapped in (scale - 5) iterations of verybig.container
+		final int lastSize = 5;
+
+		MutableComponent sizeMsg;
+		if (size <= lastSize) {
+			sizeMsg = Component.translatable("patchouli.gui.lexicon.button.resize.size" + size);
+		} else {
+			sizeMsg = Component.translatable("patchouli.gui.lexicon.button.resize.verybig.message");
+			for (int i = 0; i < size - lastSize; i++) {
+				sizeMsg = Component.translatable("patchouli.gui.lexicon.button.resize.verybig.container", sizeMsg);
+			}
+		}
+		return Arrays.asList(tooltip.get(0), sizeMsg.withStyle(ChatFormatting.GRAY));
 	}
 
 }
