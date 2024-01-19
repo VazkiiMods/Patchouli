00f8d9efb6fd620f7c41ef68699ea3ccb51b9e99
Mrbysco <Mrbysco@users.noreply.github.com>
2024-01-19 02:25:55 +0100

    Re-enable JEI support

--- Xplat/src/main/java/vazkii/patchouli/client/jei/PatchouliJeiPlugin.java
+++ Xplat/src/main/java/vazkii/patchouli/client/jei/PatchouliJeiPlugin.java
@@ -1,20 +1,32 @@
 package vazkii.patchouli.client.jei;
 
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import mezz.jei.api.runtime.IJeiRuntime;
+
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+
 import vazkii.patchouli.api.PatchouliAPI;
+import vazkii.patchouli.common.item.ItemModBook;
+import vazkii.patchouli.common.item.PatchouliItems;
 import vazkii.patchouli.mixin.client.AccessorKeyMapping;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
-//@JeiPlugin
-public class PatchouliJeiPlugin /*implements IModPlugin*/ {
+@JeiPlugin
+public class PatchouliJeiPlugin implements IModPlugin {
 	private static final ResourceLocation UID = new ResourceLocation(PatchouliAPI.MOD_ID, PatchouliAPI.MOD_ID);
 
 	private static final KeyMapping showRecipe, showUses;
 
-//	private static IJeiRuntime jeiRuntime;
+	private static IJeiRuntime jeiRuntime;
 
 	static {
 		Map<String, KeyMapping> allKeyMappings = AccessorKeyMapping.getAllKeyMappings();
@@ -25,38 +37,38 @@ public class PatchouliJeiPlugin /*implements IModPlugin*/ {
 		}
 	}
 
-//	@NotNull
-//	@Override
-//	public ResourceLocation getPluginUid() {
-//		return UID;
-//	}
+	@NotNull
+	@Override
+	public ResourceLocation getPluginUid() {
+		return UID;
+	}
 
-//	@Override
-//	public void registerItemSubtypes(@NotNull ISubtypeRegistration registration) {
-//		registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, PatchouliItems.BOOK, (stack, context) -> {
-//			if (!stack.hasTag() || !stack.getTag().contains(ItemModBook.TAG_BOOK)) {
-//				return "";
-//			}
-//			return stack.getTag().getString(ItemModBook.TAG_BOOK);
-//		});
-//	}
+	@Override
+	public void registerItemSubtypes(@NotNull ISubtypeRegistration registration) {
+		registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, PatchouliItems.BOOK, (stack, context) -> {
+			if (!stack.hasTag() || !stack.getTag().contains(ItemModBook.TAG_BOOK)) {
+				return "";
+			}
+			return stack.getTag().getString(ItemModBook.TAG_BOOK);
+		});
+	}
 
-//	@Override
-//	public void onRuntimeAvailable(@NotNull IJeiRuntime jeiRuntime) {
-//		PatchouliJeiPlugin.jeiRuntime = jeiRuntime;
-//	}
+	@Override
+	public void onRuntimeAvailable(@NotNull IJeiRuntime jeiRuntime) {
+		PatchouliJeiPlugin.jeiRuntime = jeiRuntime;
+	}
 
 	public static boolean handleRecipeKeybind(int keyCode, int scanCode, ItemStack stack) {
-//		if (showRecipe != null && showRecipe.matches(keyCode, scanCode)) {
-//			var focus = jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.OUTPUT, VanillaTypes.ITEM_STACK, stack);
-//			jeiRuntime.getRecipesGui().show(focus);
-//			return true;
-//		}
-//		if (showUses != null && showUses.matches(keyCode, scanCode)) {
-//			var focus = jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.INPUT, VanillaTypes.ITEM_STACK, stack);
-//			jeiRuntime.getRecipesGui().show(focus);
-//			return true;
-//		}
+		if (showRecipe != null && showRecipe.matches(keyCode, scanCode)) {
+			var focus = jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.OUTPUT, VanillaTypes.ITEM_STACK, stack);
+			jeiRuntime.getRecipesGui().show(focus);
+			return true;
+		}
+		if (showUses != null && showUses.matches(keyCode, scanCode)) {
+			var focus = jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.INPUT, VanillaTypes.ITEM_STACK, stack);
+			jeiRuntime.getRecipesGui().show(focus);
+			return true;
+		}
 		return false;
 	}
 }
