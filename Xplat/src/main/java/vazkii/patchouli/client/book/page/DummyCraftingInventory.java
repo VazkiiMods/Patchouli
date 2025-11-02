451f567564b21b73e5a15af2be3c094b976793a7
StoffiPoff <136234499+StoffiPoff@users.noreply.github.com>
2025-11-02 01:09:20 +0100

    Merge remote-tracking branch 'origin/fix-custom-item-model' into fix-custom-item-model
    # Conflicts:
    #	NeoForge/src/main/java/vazkii/patchouli/neoforge/client/NeoForgeClientInitializer.java
    #	Xplat/src/main/java/vazkii/patchouli/common/item/ItemModBook.java

---
+++ Xplat/src/main/java/vazkii/patchouli/client/book/page/DummyCraftingInventory.java
@@ -0,0 +1,81 @@
+package vazkii.patchouli.client.book.page;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.StackedItemContents;
+import net.minecraft.world.inventory.*;
+
+import net.minecraft.world.item.ItemStack;
+
+public final class DummyCraftingInventory implements CraftingContainer, Container {
+    public static final DummyCraftingInventory INSTANCE = new DummyCraftingInventory();
+
+    public DummyCraftingInventory() {}
+
+    @Override
+    public int getWidth() {
+        return 3;
+    }
+
+    @Override
+    public int getHeight() {
+        return 3;
+    }
+
+    @Override
+    public net.minecraft.core.NonNullList<ItemStack> getItems() {
+        return net.minecraft.core.NonNullList.withSize(getContainerSize(), ItemStack.EMPTY);
+    }
+
+    @Override
+    public int getContainerSize() {
+        return getWidth() * getHeight();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int i) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public ItemStack removeItem(int i, int j) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int i) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public void setItem(int i, ItemStack itemStack) {
+        // NO-OP
+    }
+
+    @Override
+    public void setChanged() {
+        // NO-OP
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return false;
+    }
+
+    @Override
+    public void clearContent() {
+        // NO-OP
+    }
+
+    @Override
+    public void fillStackedContents(StackedItemContents stackedItemContents) {
+        // NO-OP
+    }
+
+
+}
