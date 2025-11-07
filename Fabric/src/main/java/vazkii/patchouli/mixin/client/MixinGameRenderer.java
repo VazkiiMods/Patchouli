e43ee451d89ec65c38deb663be9833110edc6a6a
StoffiPoff <136234499+StoffiPoff@users.noreply.github.com>
2025-11-07 06:39:39 +0100

    Update MixinGameRenderer.java

--- Fabric/src/main/java/vazkii/patchouli/mixin/client/MixinGameRenderer.java
+++ Fabric/src/main/java/vazkii/patchouli/mixin/client/MixinGameRenderer.java
@@ -1,7 +1,7 @@
 package vazkii.patchouli.mixin.client;
 
 import net.minecraft.client.DeltaTracker;
-import GameRenderer;
+import net.minecraft.client.renderer.GameRenderer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
